2797cfa8a15845cc071fb18467b1ae3b184a9366
AmithRajNataraj <158771869+AmithRajNataraj@users.noreply.github.com>
2024-02-03 21:02:59 +0000

    Updated

--- convo.vhd
+++ convo.vhd
@@ -4,23 +4,20 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 
-entity conv is
+entity convolution is
     Port ( i_clk, control : in STD_LOGIC;
            i_pixel_data : in STD_LOGIC_VECTOR (71 downto 0);
            i_pixel_data_valid : in STD_LOGIC;
            o_convolved_data : out STD_LOGIC_VECTOR (7 downto 0);
            o_convolved_data_valid : out STD_LOGIC);
-end conv;
+end convolution;
 
-architecture Behavioral of conv is
+architecture Behavioral of convolution is
     type kernel1_type is array(0 to 8) of signed(7 downto 0);
     type multData1_type is array(0 to 8) of signed(16 downto 0);
-  
      
-    signal kernel1 : kernel1_type;
-    signal kernel2 : kernel1_type;
-    signal multData1 : multData1_type;
-    signal multData2 : multData1_type;
+    signal kernel1, kernel2 : kernel1_type;
+    signal multData1,multData2 : multData1_type;
     signal  sumData1, sumData2 : signed(16 downto 0);
     signal multDataValid, sumDataValid, convolved_data_int_valid : STD_LOGIC;
   
@@ -35,15 +32,7 @@ architecture Behavioral of conv is
     signal conc_i_pixel_data7 : STD_LOGIC_VECTOR(8 downto 0);
     signal conc_i_pixel_data8 : STD_LOGIC_VECTOR(8 downto 0);
     
-    signal conc_i_pixe2_data0 : STD_LOGIC_VECTOR(8 downto 0);
-    signal conc_i_pixe2_data1 : STD_LOGIC_VECTOR(8 downto 0);
-    signal conc_i_pixe2_data2 : STD_LOGIC_VECTOR(8 downto 0);
-    signal conc_i_pixe2_data3 : STD_LOGIC_VECTOR(8 downto 0); 
-    signal conc_i_pixe2_data4 : STD_LOGIC_VECTOR(8 downto 0);
-    signal conc_i_pixe2_data5 : STD_LOGIC_VECTOR(8 downto 0);
-    signal conc_i_pixe2_data6 : STD_LOGIC_VECTOR(8 downto 0);
-    signal conc_i_pixe2_data7 : STD_LOGIC_VECTOR(8 downto 0);
-    signal conc_i_pixe2_data8 : STD_LOGIC_VECTOR(8 downto 0);
+
     
 begin
     
@@ -89,15 +78,6 @@ begin
   
 
 
-conc_i_pixe2_data0 <= "0" & i_pixel_data(7 downto 0);
-conc_i_pixe2_data1 <= "0" & i_pixel_data(15 downto 8);
-conc_i_pixe2_data2 <= "0" & i_pixel_data(23 downto 16);
-conc_i_pixe2_data3 <= "0" & i_pixel_data(31 downto 24);
-conc_i_pixe2_data4 <= "0" & i_pixel_data(39 downto 32);
-conc_i_pixe2_data5 <= "0" & i_pixel_data(47 downto 40);
-conc_i_pixe2_data6 <= "0" & i_pixel_data(55 downto 48);
-conc_i_pixe2_data7 <= "0" & i_pixel_data(63 downto 56);
-conc_i_pixe2_data8 <= "0" & i_pixel_data(71 downto 64);
   
   multData2(0) <= signed(kernel2(0)) * signed(conc_i_pixel_data0);
   multData2(1) <= signed(kernel2(1)) * signed(conc_i_pixel_data1);
@@ -112,9 +92,10 @@ conc_i_pixe2_data8 <= "0" & i_pixel_data(71 downto 64);
  
 sumData1 <=  multData1(0) + multData1(1) + multData1(2) + multData1(3) + multData1(4) + multData1(5) + multData1(6) + multData1(7) + multData1(8);
 sumData2 <= multData2(0) + multData2(1) + multData2(2) + multData2(3) + multData2(4) + multData2(5) + multData2(6) + multData2(7) + multData2(8);
-   multDataValid <= i_pixel_data_valid;
-  sumDataValid <= multDataValid;
-   convolved_data_int_valid <= sumDataValid;
+
+multDataValid <= i_pixel_data_valid;
+sumDataValid <= multDataValid;
+convolved_data_int_valid <= sumDataValid;
 
     process (i_clk)
     begin
